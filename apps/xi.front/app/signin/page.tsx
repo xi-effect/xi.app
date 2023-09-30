df0ab1390355aa029d05eb399c9011ea2d067614
unknownproperty <igor.buksheff@yandex.ru>
2023-10-01 02:39:55 +0300

    fix: try to fix height on mobile

--- apps/xi.front/app/signin/page.tsx
+++ apps/xi.front/app/signin/page.tsx
@@ -9,7 +9,7 @@ export default function ResetPassword() {
   const onSignIn = useMainSt((state) => state.onSignIn);
 
   return (
-    <div className="flex flex-wrap flex-col xs:justify-center content-center w-screen h-screen">
+    <div className="flex flex-wrap flex-col xs:justify-center content-center w-screen h-[100dvh] xs:h-screen">
       <div className="flex max-w-[420px] w-full h-full xs:h-[520px] xs:border xs:border-gray-10 xs:rounded-2xl p-6 xs:p-8">
         <SignIn onSignIn={onSignIn} />
       </div>
