ce7a3c7e6174575e6fdb2937531bfeee5d025aca
Alexander Kovzan <alexanderkovzan@gmail.com>
2023-10-18 20:38:21 +0300

    feat(21136929): remove state

--- apps/xi.front/store/main.ts
+++ apps/xi.front/store/main.ts
@@ -4,13 +4,11 @@ import { create } from 'zustand';
 import { UserProfile, createUserProfileSt } from './user/userProfile';
 import { UserSettings, createUserSettingsSt } from './user/userSettings';
 import { SignIn, createSignInSt } from './auth/signin';
-import { PasswordRecovery,createPasswordRecoverySt } from './auth/password-recovery';
 
-export type Common = UserProfile & UserSettings & SignIn & PasswordRecovery;
+export type Common = UserProfile & UserSettings & SignIn;
 
 export const useMainSt = create<Common>()((...a) => ({
   ...createUserProfileSt(...a),
   ...createUserSettingsSt(...a),
-  ...createSignInSt(...a),
-  ...createPasswordRecoverySt(...a)
+  ...createSignInSt(...a)
 }));
