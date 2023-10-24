7e62ccc8605aa2fbd3c5bfdbc44b41638cf10020
Alexander Kovzan <alexanderkovzan@gmail.com>
2023-10-24 12:09:16 +0300

    fix(21136929): fix border style

--- apps/xi.front/app/reset-password/page.tsx
+++ apps/xi.front/app/reset-password/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function ResetPasswordPage() {
   return (
     <div className="flex flex-wrap flex-col justify-start xs:justify-center content-center w-screen h-screen p-1">
-      <div className="flex max-w-[420px] w-full h-full xs:h-[520px]  border-none xs:border-solid xs:border xs:border-gray-10 rounded-2xl p-6 xs:p-8">
+      <div className="flex max-w-[420px] w-full h-full xs:h-[520px]  border-0 border-gray-10 xs:border rounded-2xl p-6 xs:p-8">
         <ResetPassword />
       </div>
     </div>
