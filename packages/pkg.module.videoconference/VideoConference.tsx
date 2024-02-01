6adbfac657d140fe660771f8261b1e04c1b359a7
unknownproperty <igor.buksheff@yandex.ru>
2024-02-02 01:10:49 +0300

    feat: try to VideoConf

--- packages/pkg.module.videoconference/VideoConference.tsx
+++ packages/pkg.module.videoconference/VideoConference.tsx
@@ -1,16 +1,8 @@
 'use client';
 
-// import '@livekit/components-styles';
-import {
-  ControlBar,
-  GridLayout,
-  LiveKitRoom,
-  ParticipantTile,
-  RoomAudioRenderer,
-  useTracks,
-} from '@livekit/components-react';
+import { LiveKitRoom, RoomAudioRenderer } from '@livekit/components-react';
 import { Grid, External, Settings } from '@xipkg/icons';
-import { Track } from 'livekit-client';
+import { MyVideoConference } from './components/MyVideoConference';
 import { BottomBar } from './components/BottomBar';
 
 const serverUrl = 'wss://livekit.xieffect.ru';
@@ -57,22 +49,3 @@ export const VideoConference = ({ token }: VideoConferenceT) => {
     </LiveKitRoom>
   );
 };
-
-function MyVideoConference() {
-  // `useTracks` returns all camera and screen share tracks. If a user
-  // joins without a published camera track, a placeholder track is returned.
-  const tracks = useTracks(
-    [
-      { source: Track.Source.Camera, withPlaceholder: true },
-      { source: Track.Source.ScreenShare, withPlaceholder: false },
-    ],
-    { onlySubscribed: false },
-  );
-  return (
-    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
-      {/* The GridLayout accepts zero or one child. The child is used
-      as a template to render all passed in tracks. */}
-      <ParticipantTile />
-    </GridLayout>
-  );
-}
