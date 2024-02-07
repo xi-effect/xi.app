a0e36518be045dacff52c0d7ed33ab8fa7d139ba
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-02-07 17:51:00 +0100

    feat(28612812): add breakpoints for 375px, 700px and 1440px

--- apps/xi.front/app/community/[cid]/home/page.tsx
+++ apps/xi.front/app/community/[cid]/home/page.tsx
@@ -4,21 +4,26 @@ import { Button } from '@xipkg/button';
 import { Telegram, TaskFile } from '@xipkg/icons';
 
 import Image from 'next/image';
-import Link from 'next/link';
 
 function Header() {
   const role = 'администратор';
 
   return (
-    <header className="pb-8 w-[100%] max-w-[1570px]">
-      <div className="flex font-semibold text-[40px] leading-[48px]">
+    <header className=" max-xs:pb-4 pb-8 w-[100%] max-w-[1570px]">
+      <div className="font-semibold text-[32px] max-xs:text-2xl leading-10 xl:text-[40px] xl:leading-[48px] xl:flex">
         <h2>Добро пожаловать в сообщество</h2>
-        <div className="flex ml-6">
-          <Image src="/assets/avatarrep.svg" alt="Аватар пользователя" width={48} height={48} />
-          <p className="ml-4">Иванова А. Г.</p>
+        <div className="flex items-center max-xl:mt-3 max-xs:mt-0 xl:ml-6">
+          <Image
+            className=" max-xs:w-6 max-xs:h-6 w-8 h-8 xl:h-12 xl:w-12"
+            src="/assets/avatarrep.svg"
+            alt="Аватар пользователя"
+            width={48}
+            height={0}
+          />
+          <p className="ml-2 xl:ml-4">Иванова А. Г.</p>
         </div>
       </div>
-      <p className="mt-4 font-normal text-[#404040] text-2xl">
+      <p className="mt-4 font-normal max-xs:mt-2 max-xs:text-sm text-[#404040] text-[16px] leading-[22px] xl:text-2xl">
         Ваша роль: {role || `[РОЛЬ ПОЛЬЗОВАТЕЛЯ]`}
       </p>
     </header>
@@ -27,36 +32,36 @@ function Header() {
 
 function InfoCardList() {
   return (
-    <ul className="grid grid-cols-3 py-8 gap-12 max-w-[1570px]">
-      <li>
-        <div className="w-full bg-gray-5 overflow-hidden h-60 rounded-2xl">
+    <ul className="grid py-8 max-xs:py-4 gap-12 max-w-[1570px] xl:grid-cols-3">
+      <li className="overflow-hidden">
+        <div className="flex items-end max-xl:justify-center justify-end w-full min-w-[470px] bg-gray-5 h-60 rounded-2xl">
           <Image
-            className="min-w-[470px] h-auto"
+            className="h-auto"
             src={'/assets/community-home-page/left-menu-screenshot.png'}
-            width={0}
+            width={422}
             height={0}
             sizes="100vw"
             alt={`Изображение 'Меню слева' не загрузилось`}
           />
         </div>
-        <h3 className="font-semibold text-2xl mt-4">Меню слева</h3>
-        <p className="font-normal text-base mt-2 text-[#101010]">
+        <h3 className="font-semibold text-2xl max-xs:text-xl mt-4">Меню слева</h3>
+        <p className="font-normal text-base max-xs:text-sm max-xs:mt-1 mt-2 text-[#101010]">
           С помощью меню слева вы можете перемещаться по каналам внутри сообщества
         </p>
       </li>
-      <li>
-        <div className="w-full bg-gray-5 overflow-hidden h-60 rounded-2xl">
+      <li className="overflow-hidden">
+        <div className="w-full xl:min-w-[470px] bg-gray-5 h-60 rounded-2xl flex items-center justify-center">
           <Image
-            className="min-w-[470px] h-auto"
+            className=" h-auto"
             src={'/assets/community-home-page/roles-screenshot.png'}
-            width={0}
-            height={0}
+            width={236}
+            height={92}
             sizes="100vw"
             alt={`Изображение 'Роли' не загрузилось`}
           />
         </div>
-        <h3 className="font-semibold text-2xl mt-4">Роли</h3>
-        <p className="font-normal text-base mt-2 text-[#101010]">
+        <h3 className="font-semibold text-2xl max-xs:text-xl mt-4">Роли</h3>
+        <p className="font-normal text-base max-xs:text-sm max-xs:mt-1 mt-2 text-[#101010]">
           В рамках сообщества могут взаимодейтсвовать только студенты и преподаватели
         </p>
       </li>
@@ -71,8 +76,8 @@ function InfoCardList() {
             alt={`Изображение 'Смена сообщества' не загрузилось`}
           />
         </div>
-        <h3 className="font-semibold text-2xl mt-4">Смена сообщества</h3>
-        <p className="font-normal text-base mt-2 text-[#101010]">
+        <h3 className="font-semibold text-2xl max-xs:text-xl mt-4">Смена сообщества</h3>
+        <p className="font-normal text-base max-xs:text-sm max-xs:mt-1 mt-2 text-[#101010]">
           Для перехода в другое сообщество выберите его из выпадающего списка в меню слева
         </p>
       </li>
@@ -82,52 +87,37 @@ function InfoCardList() {
 
 function SupportBox() {
   return (
-    <div className="bg-[#F7F7F7] p-8 rounded-2xl my-8 w-[100%] max-w-[1570px]">
-      <div className="flex gap-1">
-        <h3 className="font-semibold text-[40px] leading-[48px]">Мы всегда рядом</h3>
-        <span className="bg-[url('/assets/community-home-page/support-box-heart.svg')] bg-no-repeat bg-center w-[18px] mb-2" />
+    <section className="py-8 max-xs:py-4  w-[100%] max-w-[1570px]">
+      <div className="bg-[#F7F7F7] p-8 rounded-2xl w-[100%]">
+        <div className="flex gap-1">
+          <h3 className="font-semibold text-[32px] max-xs:text-2xl leading-10 xl:text-[40px] xl:leading-[48px]">
+            Мы всегда рядом
+          </h3>
+          <span className="bg-[url('/assets/community-home-page/support-box-heart.svg')] bg-no-repeat bg-center w-[15px] max-xs:w-[12px] xl:w-[18px] mb-2 bg-contain" />
+        </div>
+        <p className="text-sm max-xs:text-xs xl:text-xl text-[#404040] font-normal mt-2 max-w-[800px]">
+          Наша команда всегда на связи и готова ответить на любые вопросы. А ещё мы рады обратной
+          связи, вашим идеям и предложениям.
+        </p>
+        <div className=" max-xs:flex-col max-xs:gap-2 flex gap-8 mt-6">
+          <Button className=" text-sm h-8 xl:h-11 xl:text-base font-medium">
+            Написать в телеграм{' '}
+            <Telegram
+              className="fill-white ml-[6px] xl:ml-2 w-4 h-4 xl:w-6 xl:h-6"
+              width={0}
+              height={0}
+            />
+          </Button>
+          <Button
+            variant={'secondary'}
+            className="text-sm h-8 xl:h-11 xl:text-base border-[1px] xl:border-2 font-medium"
+          >
+            Посмотреть руководства{' '}
+            <TaskFile className="ml-[6px] w-4 h-4 xl:w-6 xl:h-6 xl:ml-2" width={24} height={24} />
+          </Button>
+        </div>
       </div>
-      <p className="text-xl text-[#404040] font-normal mt-2 max-w-[800px]">
-        Наша команда всегда на связи и готова ответить на любые вопросы. А ещё мы рады обратной
-        связи, вашим идеям и предложениям.
-      </p>
-      <div className="flex gap-8 mt-6">
-        <Button className="text-base font-medium">
-          Написать в телеграм <Telegram className="fill-white ml-2" width={24} height={24} />
-        </Button>
-        <Button variant={'secondary'} className="text-base font-medium">
-          Посмотреть руководства <TaskFile className="ml-2" width={24} height={24} />
-        </Button>
-      </div>
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="flex justify-between mt-auto items-center w-[100%] max-w-[1570px]">
-      <div className="flex gap-2">
-        <Image src={'/assets/brand/navigationlogo.svg'} alt="Логотип" width={67.25} height={8} />
-        <p className="text-xs font-normal text-[#282828]">с 2020 года</p>
-      </div>
-      <ul className="flex gap-8">
-        <li>
-          <Link href={'#'} className="text-xs font-normal text-[#282828]">
-            Поддержка
-          </Link>
-        </li>
-        <li>
-          <Link href={'#'} className="text-xs font-normal text-[#282828]">
-            Политика конфиденциальности
-          </Link>
-        </li>
-        <li>
-          <Link href={'#'} className="text-xs font-normal text-[#282828]">
-            Пользовательское соглашение
-          </Link>
-        </li>
-      </ul>
-    </footer>
+    </section>
   );
 }
 
@@ -137,7 +127,6 @@ export default function CommunityHomePage() {
       <Header />
       <InfoCardList />
       <SupportBox />
-      <Footer />
     </div>
   );
 }
