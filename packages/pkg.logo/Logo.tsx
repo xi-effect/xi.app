cf1f6d4aab6d7686658fc8865e4bb3b61f07d3df
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-03-01 16:08:18 +0100

    feat(29808190): add pkg.logo; add logo svg to /public

---
+++ packages/pkg.logo/Logo.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import React from "react"
+import Image from "next/image";
+import { useTheme } from "next-themes"
+
+type LogoProps = {
+    height?: number,
+    width?: number,
+    
+    //  logoVarian это название файла svg (без расширения),
+    //  в папке public/assets/brand 
+    logoVariant?: 'navigation' | 'id'
+    logoSize?: 'default' | 'small'
+}
+
+export const Logo = ({height = 22, width = 180, logoVariant, logoSize}: LogoProps) => {
+   const { resolvedTheme } = useTheme()
+
+   if (resolvedTheme === 'light') return (
+          <Image
+            height={height}
+            width={width}
+            alt="xieffect logo"
+            src={`/assets/brand/${logoVariant}logo-${logoSize}-light.svg`}
+          />
+    )
+
+    return (
+          <Image
+            height={height}
+            width={width}
+            alt="xieffect logo"
+            src={`/assets/brand/${logoVariant}logo-${logoSize}-dark.svg`}
+          />
+    )
+    
+}
