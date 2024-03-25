33b60d6c1b420f4cc71c6c65b6f05b15a344473d
unknownproperty <igor.buksheff@yandex.ru>
2024-03-25 23:56:37 +0300

    fix: withNodeId

--- packages/pkg.module.editor/plugins/withNodeId.ts
+++ packages/pkg.module.editor/plugins/withNodeId.ts
@@ -6,29 +6,29 @@ import { nanoid } from 'nanoid';
 export const makeNodeId = () => nanoid(16);
 
 export const assignIdRecursively = (node: any) => {
-    if (Element.isElement(node)) {
-        // @ts-ignore
-        node.id = makeNodeId();
-        node.children.forEach(assignIdRecursively);
-    }
+  if (Element.isElement(node)) {
+    // @ts-ignore
+    node.id = makeNodeId();
+    node.children.forEach(assignIdRecursively);
+  }
 };
 
 export const withNodeId = (editor: any) => {
-    const { apply } = editor;
+  const { apply } = editor;
 
-    editor.apply = (operation: any) => {
-        if (operation.type === 'insert_node') {
-            assignIdRecursively(operation.node);
-            return apply(operation);
-        }
+  editor.apply = (operation: any) => {
+    if (operation.type === 'insert_node') {
+      assignIdRecursively(operation.node);
+      return apply(operation);
+    }
 
-        if (operation.type === 'split_node') {
-            operation.properties.id = makeNodeId();
-            return apply(operation);
-        }
+    if (operation.type === 'split_node') {
+      operation.properties.id = makeNodeId();
+      return apply(operation);
+    }
 
-        return apply(operation);
-    };
+    return apply(operation);
+  };
 
-    return editor;
+  return editor;
 };
