2e7331ef6377858f0f5aff8769e86ad1c166cefc
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-03-29 08:39:02 +0100

    fix(29219183): move debauncers to xi.kit

--- packages/pkg.utils/index.tsx
+++ packages/pkg.utils/index.tsx
@@ -1,17 +1,5 @@
 import { get, put, post, patch, del } from './fetch';
 import { useMedia } from './useMedia';
 import { useSessionStorage } from './useSessionStorage';
-import { useDebounce } from './useDebounce';
-import { useDebouncedFunction } from './useDebouncedFunction';
 
-export {
-  get,
-  put,
-  post,
-  patch,
-  del,
-  useMedia,
-  useSessionStorage,
-  useDebounce,
-  useDebouncedFunction,
-};
+export { get, put, post, patch, del, useMedia, useSessionStorage };
