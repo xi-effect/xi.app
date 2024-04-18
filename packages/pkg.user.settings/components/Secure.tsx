a51bade5147fcaaef9fd652c7dbd613d04befe11
Julia Bednaia <juliabednaia@gmail.com>
2024-04-18 13:25:10 +0300

    fix(32270064): added changes to Secure.tsx layout

--- packages/pkg.user.settings/components/Secure.tsx
+++ packages/pkg.user.settings/components/Secure.tsx
@@ -15,42 +15,44 @@ export const Secure = () => {
 
   return (
     <>
-      {!isMobile && <span className="text-3xl font-semibold">Безопасность</span>}
-      <ConfirmEmail buttonText="Оправить повторно" />
-      <div className="border-gray-80 flex w-full flex-col rounded-2xl border p-1 sm:mt-4">
-        <div className="flex w-full flex-col p-3">
-          <span className="text-xl font-semibold">Данные аккаунта</span>
-          <span className="text-sm font-normal">Видны только вам</span>
+      {!isMobile && <h1 className="text-3xl font-semibold mb-4">Безопасность</h1>}
+      <div className="flex flex-col gap-6 sm:gap-8">
+        <ConfirmEmail buttonText="Оправить повторно" />
+        <div className="border-gray-80 flex w-full flex-col rounded-2xl border p-1">
+          <div className="flex w-full flex-col p-3">
+            <span className="text-xl font-semibold">Данные аккаунта</span>
+            <span className="text-sm font-normal">Видны только вам</span>
+          </div>
+          <ChangePasswordModal
+            open={isChangePasswordModalOpen}
+            onOpenChange={setIsChangePasswordModalOpen}
+          >
+            <button
+              type="button"
+              className="hover:bg-gray-5 flex h-[66px] flex-row items-center gap-4 rounded-xl bg-transparent p-3"
+            >
+              <Key size="l" className="fill-brand-80" />
+              <div className="items-star flex flex-col">
+                <span className="w-fit font-semibold">Пароль</span>
+                <span className="text-xs font-normal">Обновлён год назад</span>
+              </div>
+              <ChevronRight className="fill-gray-80 ml-auto" />
+            </button>
+          </ChangePasswordModal>
+          <ChangeEmailModal>
+            <button
+              type="button"
+              className="hover:bg-gray-5 flex h-[66px] flex-row items-center gap-4 rounded-xl bg-transparent p-3"
+            >
+              <Mail size="l" className="fill-brand-80" />
+              <div className="items-star flex flex-col">
+                <span className="w-fit font-semibold">Почта</span>
+                <span className="text-xs font-normal">{user.email}</span>
+              </div>
+              <ChevronRight className="fill-gray-80 ml-auto" />
+            </button>
+          </ChangeEmailModal>
         </div>
-        <ChangePasswordModal
-          open={isChangePasswordModalOpen}
-          onOpenChange={setIsChangePasswordModalOpen}
-        >
-          <button
-            type="button"
-            className="hover:bg-gray-5 flex h-[66px] flex-row items-center gap-4 rounded-xl bg-transparent p-3"
-          >
-            <Key size="l" className="fill-brand-80" />
-            <div className="items-star flex flex-col">
-              <span className="w-fit font-semibold">Пароль</span>
-              <span className="text-xs font-normal">Обновлён год назад</span>
-            </div>
-            <ChevronRight className="fill-gray-80 ml-auto" />
-          </button>
-        </ChangePasswordModal>
-        <ChangeEmailModal>
-          <button
-            type="button"
-            className="hover:bg-gray-5 flex h-[66px] flex-row items-center gap-4 rounded-xl bg-transparent p-3"
-          >
-            <Mail size="l" className="fill-brand-80" />
-            <div className="items-star flex flex-col">
-              <span className="w-fit font-semibold">Почта</span>
-              <span className="text-xs font-normal">{user.email}</span>
-            </div>
-            <ChevronRight className="fill-gray-80 ml-auto" />
-          </button>
-        </ChangeEmailModal>
       </div>
     </>
   );
