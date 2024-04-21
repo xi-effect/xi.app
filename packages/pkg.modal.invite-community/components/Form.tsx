1fded53074a94cd6599d3b0eb3b0907462eb287b
Vitaly Kharlakov <vkharlakov@Vitalys-MacBook-Air.local>
2024-04-21 21:57:08 +0200

    fix(31224398) fixed prev commit

--- packages/pkg.modal.invite-community/components/Form.tsx
+++ packages/pkg.modal.invite-community/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import * as z from 'zod';
 import * as M from '@xipkg/modal';
@@ -26,10 +26,25 @@ import {
   DropdownMenuTrigger,
 } from '@xipkg/dropdown';
 
+const RoleSchema = z.object({
+  name: z.string(),
+  bgColorMain: z.string(),
+  bgColorSecondary: z.string(),
+});
+
 const FormSchema = z.object({
   maxUsageCount: z.string(),
-  date: z.string().datetime({ offset: true }).nullish().or(z.string().max(0)),
-  roles: z.array(z.string()),
+  date: z
+    .string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/)
+    .nullish()
+    .or(z.string().max(0)),
+  time: z
+    .string()
+    .regex(/^([01]\d|2[0-3]):([0-5]\d)$/)
+    .nullish()
+    .or(z.string().max(0)),
+  roles: z.array(RoleSchema),
 });
 
 export default function FormBlock() {
@@ -43,6 +58,12 @@ export default function FormBlock() {
     { name: '2', bgColorMain: 'bg-gray-80', bgColorSecondary: 'bg-gray-5' },
   ];
 
+  // Создаю список выбора часов для дропдауна
+  const timeOptions = Array.from({ length: 24 }, (_, i) => {
+    const hour = i.toString().padStart(2, '0');
+    return `${hour}:00`;
+  });
+
   const [date, setDate] = useState<Date>();
   const [unusedRoles, setUnusedRoles] = useState<typeof rolesTemplate>(rolesTemplate);
 
@@ -51,6 +72,7 @@ export default function FormBlock() {
     defaultValues: {
       maxUsageCount: '',
       date: '',
+      time: '',
       roles: [],
     },
   });
@@ -89,55 +111,91 @@ export default function FormBlock() {
               </FormItem>
             )}
           />
-          <FormField
-            name="date"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Ограничение по времени</FormLabel>
-                <FormDescription className="text-sm">
-                  Для приглашения без ограничения оставить пустым
-                </FormDescription>
-                <FormControl>
-                  <DatePicker
-                    popoverProps={{ modal: true }}
-                    popoverTriggerProps={{}}
-                    popoverContentProps={{}}
-                    mode="single"
-                    className="mt-2"
-                    selected={date}
-                    onSelect={(selectedDate: Date | undefined) => {
-                      if (selectedDate) {
-                        form.setValue(
-                          'date',
-                          format(selectedDate, "yyyy-MM-dd'T'HH:mm:ssxxx", { locale: ru }),
-                        );
-                        setDate(selectedDate);
-                      }
-                    }}
-                  >
-                    <Input
-                      className="cursor-pointer"
-                      {...field}
-                      value={
-                        form.getValues('date')
-                          ? format((form.getValues('date') ?? '').toString(), 'PPP', { locale: ru })
-                          : ''
-                      }
-                      after={<Calendar className="fill-gray-60 h-6 w-6" />}
-                    />
-                  </DatePicker>
-                </FormControl>
-                <Button
-                  type="reset"
-                  onClick={() => form.resetField('date')}
-                  className="text-gray-60 h-min w-min rounded-none bg-transparent p-0 text-xs hover:bg-transparent hover:underline focus:bg-transparent active:bg-transparent"
-                >
-                  Очистить
-                </Button>
-              </FormItem>
-            )}
-          />
+          <div className="flex gap-2">
+            <FormField
+              name="date"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Ограничение по времени</FormLabel>
+                  <FormDescription className="text-sm">
+                    Для приглашения без ограничения оставить пустым
+                  </FormDescription>
+                  <FormControl>
+                    <DatePicker
+                      popoverProps={{ modal: true }}
+                      popoverTriggerProps={{}}
+                      popoverContentProps={{}}
+                      mode="single"
+                      className="mt-2 w-full"
+                      selected={date}
+                      onSelect={(selectedDate: Date | undefined) => {
+                        if (selectedDate) {
+                          form.setValue(
+                            'date',
+                            format(selectedDate, 'yyyy-MM-dd', {
+                              locale: ru,
+                            }),
+                          );
+                          setDate(selectedDate);
+                        }
+                      }}
+                    >
+                      <Input
+                        className="cursor-pointer"
+                        {...field}
+                        value={
+                          form.getValues('date')
+                            ? format((form.getValues('date') ?? '').toString(), 'PPP', {
+                                locale: ru,
+                              })
+                            : ''
+                        }
+                        after={<Calendar className="fill-gray-60 h-6 w-6" />}
+                      />
+                    </DatePicker>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="time"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="mt-auto">
+                  <FormControl aria-disabled>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Input
+                          {...field}
+                          className={`w-[100px] cursor-pointer disabled:pointer-events-none`}
+                          disabled={form.getValues('date') === ''}
+                          value={
+                            form.getValues('date') && (form.getValues('time') || '00:00')
+                              ? form.getValues('time') || '00:00'
+                              : ''
+                          }
+                        />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="max-h-[200px] overflow-scroll">
+                        {timeOptions.map((option, index) => (
+                          <DropdownMenuItem
+                            key={index}
+                            className="hover:bg-gray-10 rounded-lg"
+                            onClick={() => {
+                              form.setValue('time', option);
+                            }}
+                          >
+                            <p>{option}</p>
+                          </DropdownMenuItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
           <FormField
             name="roles"
             control={form.control}
@@ -152,6 +210,14 @@ export default function FormBlock() {
                           placeholder="Выберите роль"
                           className="text-gray-0 cursor-pointer"
                           {...field}
+                          value={
+                            form.getValues('roles')
+                              ? form
+                                  .getValues('roles')
+                                  .map((role) => role.name)
+                                  .join(', ')
+                              : ''
+                          }
                           after={
                             <Plus className="bg-brand-80 fill-gray-0 absolute right-0 h-6 w-6 rounded-lg p-1" />
                           }
@@ -163,10 +229,7 @@ export default function FormBlock() {
                             key={index}
                             className="hover:bg-gray-10 rounded-lg"
                             onClick={() => {
-                              form.setValue('roles', [
-                                ...form.getValues('roles'),
-                                roleTemplate.name,
-                              ]);
+                              form.setValue('roles', [...form.getValues('roles'), roleTemplate]);
                               setUnusedRoles(unusedRoles.filter((role) => role !== roleTemplate));
                             }}
                           >
@@ -184,35 +247,32 @@ export default function FormBlock() {
                       {form.getValues('roles').map((role, index) => (
                         <li
                           key={index}
-                          className="bg-gray-5 flex items-center gap-2 rounded-md px-2 py-1 text-xs"
+                          className={`${role.bgColorSecondary} flex items-center gap-2 rounded-md px-2 py-1 text-xs`}
                         >
-                          <span className="bg-gray-80 inline-block size-3 rounded-full" />
-                          {role}
+                          <span
+                            className={`${role.bgColorMain} inline-block size-3 rounded-full`}
+                          />
+                          {role.name}
                         </li>
                       ))}
                     </ul>
                   )}
                 </div>
-                <Button
-                  type="reset"
-                  onClick={() => {
-                    form.resetField('roles');
-                    setUnusedRoles(rolesTemplate);
-                  }}
-                  className="text-gray-60 h-min w-min rounded-none bg-transparent p-0 text-xs hover:bg-transparent hover:underline focus:bg-transparent active:bg-transparent"
-                >
-                  Очистить
-                </Button>
               </FormItem>
             )}
           />
         </div>
-        <M.ModalFooter className="flex justify-end gap-4">
-          <Button variant="secondary" className="h-12 px-6" type="button">
-            Отменить
-          </Button>
-          <Button className="h-12 px-6" type="submit">
-            Создать
+        <M.ModalFooter className="flex justify-between">
+          <div className="flex gap-4">
+            <Button className="h-12 px-6" type="submit">
+              Создать
+            </Button>
+            <Button variant="secondary" className="h-12 px-6" type="button">
+              Отменить
+            </Button>
+          </div>
+          <Button type="reset" variant="ghost" onClick={() => form.reset()} className="h-12 px-6">
+            Очистить
           </Button>
         </M.ModalFooter>
       </form>
