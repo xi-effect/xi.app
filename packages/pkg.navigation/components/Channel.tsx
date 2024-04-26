adf6b412c1811088329daf4cc36cc6c16a380cc4
Ley official <danyilfreelance@gmail.com>
2024-04-26 21:01:24 +0100

    fix 323448355 : gcodereview buggs

--- packages/pkg.navigation/components/Channel.tsx
+++ packages/pkg.navigation/components/Channel.tsx
@@ -1,86 +1,82 @@
 import { ReactNode, useState } from 'react';
-import { IChannel } from './types'
+import { IChannel } from './types';
 import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from "@dnd-kit/utilities";
+import { CSS } from '@dnd-kit/utilities';
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { Announce, Calendar, Chat, Conference, Home, Task, Updates , Move} from '@xipkg/icons';
-
+import { Announce, Calendar, Chat, Conference, Home, Task, Updates, Move } from '@xipkg/icons';
 
 interface IChannelProps {
-    channel : IChannel;
-    setSlideIndex? : (arg : number) => void
+  channel: IChannel;
+  setSlideIndex?: (arg: number) => void;
 }
 
 interface IIconsDict {
   [key: string]: ReactNode;
+}
+
+const iconClassName = 'transition-colors ease-in group-hover:fill-brand-80';
+
+const iconsDict: IIconsDict = {
+  announce: <Announce className={iconClassName} />,
+  calendar: <Calendar className={iconClassName} />,
+  updates: <Updates className={iconClassName} />,
+  task: <Task className={iconClassName} />,
+  chat: <Chat className={iconClassName} />,
+  camera: <Conference className={iconClassName} />,
+  home: <Home className={iconClassName} />,
 };
 
-  const iconClassName = 'transition-colors ease-in group-hover:fill-brand-80';
+export function Channel({ channel, setSlideIndex }: IChannelProps) {
+  const [mouseOver, setMouseOver] = useState(false);
+  const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+    id: channel.elId,
+    data: {
+      type: 'Channel',
+      channel,
+    },
+  });
 
-  const iconsDict: IIconsDict = {
-    announce: <Announce className={iconClassName} />,
-    calendar: <Calendar className={iconClassName} />,
-    updates: <Updates className={iconClassName} />,
-    task: <Task className={iconClassName} />,
-    chat: <Chat className={iconClassName} />,
-    camera: <Conference className={iconClassName} />,
-    home : <Home className={iconClassName}/>
+  const router = useRouter();
+
+  const handleRouteChange = () => {
+    setSlideIndex && setSlideIndex(1);
+    router.push(channel.link);
   };
 
-export function Channel({channel , setSlideIndex } : IChannelProps) {  
-  const [mouseOver , setMouseOver] = useState(false);
-  const {
-        setNodeRef,
-        attributes,
-        listeners,
-        transform,
-        transition,
-        isDragging, 
-      } = useSortable({
-        id: channel.elId,
-        data: {
-          type: "Channel",
-          channel,
-        },
-      });
+  const style = {
+    height: '40px',
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
 
-      const router = useRouter();
-  
-      const handleRouteChange = () => {
-      setSlideIndex && setSlideIndex(1);
-      router.push(channel.link);
-      };
-      
+  if (isDragging) {
+    return (
+      <div ref={setNodeRef} style={style}>
+        <div className="bg-brand-80 h-[4px] rounded-[2px]"></div>
+      </div>
+    );
+  }
 
-      const style = {
-        height : '40px',
-        transition,
-        transform: CSS.Transform.toString(transform),
-      };
-
-      if (isDragging) {
-        return <div ref={setNodeRef}  style={style}>
-          <div className="h-[4px] rounded-[2px] border-b-brand-80 bg-brand-80"></div>
+  return (
+    <div
+      onMouseEnter={() => setMouseOver(true)}
+      onMouseLeave={() => setMouseOver(false)}
+      ref={setNodeRef}
+      style={style}
+      onClick={() => handleRouteChange()}
+    >
+      <div className="text-gray-90 hover:bg-brand-0 hover:text-brand-80 group flex h-[40px] w-full flex-row items-center justify-between rounded-lg p-2 transition-colors ease-in hover:cursor-pointer">
+        <div className="flex items-center">
+          {iconsDict[channel.icon]}
+          <span className="pl-2 text-[14px] font-normal">{channel.label}</span>
         </div>
-      }
-
-      return (
-        <div 
-        onMouseEnter={() => setMouseOver(true)}
-        onMouseLeave={() => setMouseOver(false)}
-          ref={setNodeRef}
-          style={style}
-        
-          onClick={() => handleRouteChange() }
-        >
-            <div className='justify-between text-gray-90 hover:bg-brand-0 hover:text-brand-80 group flex h-[40px] w-full flex-row items-center rounded-lg p-2 transition-colors ease-in hover:cursor-pointer'>
-                <div className='flex items-center'>
-                {iconsDict[channel.icon]}
-                 <span className="pl-2 text-[14px] font-normal">{channel.label}</span>
-                </div>
-                {mouseOver ? <div {...attributes} {...listeners}> <Move/></div> : null}
-            </div>
-        </div>
-      );
+        {mouseOver ? (
+          <div {...attributes} {...listeners}>
+            <Move />
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
--- packages/pkg.navigation/components/CommunityItems.tsx
+++ packages/pkg.navigation/components/CommunityItems.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-
 'use client';
 
 import { useMemo, useState } from 'react';
-import { CategoryContainer } from './CategoryContainer'
-import { IChannel, ICategory } from './types'
+import { CategoryContainer } from './CategoryContainer';
+import React from 'react';
+import { IChannel, ICategory } from './types';
 import {
   DndContext,
   useSensors,
@@ -18,33 +16,33 @@ import {
   MeasuringStrategy,
   closestCorners,
 } from '@dnd-kit/core';
-import { arrayMove } from "@dnd-kit/sortable";
+import { arrayMove } from '@dnd-kit/sortable';
 import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { Channel } from './Channel';
 import { createPortal } from 'react-dom';
 
-let defaultCategories: ICategory[] = [
+const defaultCategories: ICategory[] = [
   {
-    title : '',
-    subtitle : '',
-    id : 'empty',
+    title: '',
+    subtitle: '',
+    id: 'empty',
   },
   {
     title: 'B1.2',
     subtitle: 'Intermediate',
-    id: "B1.2",
+    id: 'B1.2',
   },
   {
     title: 'B2.0',
     subtitle: 'Intermediate',
-    id: "B2.0",
+    id: 'B2.0',
   },
 ];
 
-let defaultChannels: IChannel[] = [
+const defaultChannels: IChannel[] = [
   {
-    elId: "1",
-    categoryId: "B1.2",
+    elId: '1',
+    categoryId: 'B1.2',
     icon: 'announce',
     type: 'announce',
     label: 'Объявления',
@@ -54,7 +52,7 @@ let defaultChannels: IChannel[] = [
     elId: '2',
     icon: 'task',
     type: 'task',
-    categoryId : 'B1.2',
+    categoryId: 'B1.2',
     label: 'Задания',
     link: '/community/1/task/1',
   },
@@ -62,7 +60,7 @@ let defaultChannels: IChannel[] = [
     elId: '3',
     icon: 'chat',
     type: 'chat',
-    categoryId : 'B1.2',
+    categoryId: 'B1.2',
     label: 'Чат',
     link: '/community/1/chat/1',
   },
@@ -70,14 +68,14 @@ let defaultChannels: IChannel[] = [
     elId: '4',
     icon: 'camera',
     type: 'videoconference',
-    categoryId : 'B1.2',
+    categoryId: 'B1.2',
     label: 'Видеоконференция',
     link: '/community/1/videoconference/1',
   },
   {
     elId: '5',
     icon: 'announce',
-    categoryId : 'B2.0',
+    categoryId: 'B2.0',
     type: 'announce',
     label: 'Объявления',
     link: '/community/1/announce/1',
@@ -86,7 +84,7 @@ let defaultChannels: IChannel[] = [
     elId: '6',
     icon: 'task',
     type: 'task',
-    categoryId : 'B2.0',
+    categoryId: 'B2.0',
     label: 'Задания',
     link: '/community/1/task/1',
   },
@@ -94,14 +92,14 @@ let defaultChannels: IChannel[] = [
     elId: '7',
     icon: 'chat',
     type: 'chat',
-    categoryId : 'B2.0',
+    categoryId: 'B2.0',
     label: 'Чат',
     link: '/community/1/chat/1',
   },
   {
     elId: '8',
     icon: 'camera',
-    categoryId : 'B2.0',
+    categoryId: 'B2.0',
     type: 'videoconference',
     label: 'Видеоконференция',
     link: '/community/1/videoconference/1',
@@ -109,7 +107,7 @@ let defaultChannels: IChannel[] = [
   {
     elId: '9',
     icon: 'home',
-    categoryId : 'empty',
+    categoryId: 'empty',
     type: 'home',
     label: 'Главная',
     link: '/community/1/home',
@@ -117,7 +115,7 @@ let defaultChannels: IChannel[] = [
   {
     elId: '10',
     icon: 'announce',
-    categoryId : 'empty',
+    categoryId: 'empty',
     type: 'announce',
     label: 'Объявления',
     link: '',
@@ -125,24 +123,23 @@ let defaultChannels: IChannel[] = [
   {
     elId: '11',
     icon: 'calendar',
-    categoryId : 'empty',
+    categoryId: 'empty',
     type: 'calendar',
     label: 'Календарь',
     link: '',
   },
-
 ];
 
 interface ICommunityItems {
   setSlideIndex: (value: number) => void;
   className?: string;
-};
+}
 
 export const CommunityItems = ({ className, setSlideIndex }: ICommunityItems) => {
   const [categories, setCategories] = useState<ICategory[]>(defaultCategories);
   const [channels, setChannels] = useState<IChannel[]>(defaultChannels);
   const categoryIds = useMemo(() => categories.map((cat) => cat.id), [categories]);
-  const channelsIds = useMemo(() => channels.map((channel) => channel.elId) , [channels])
+  const channelsIds = useMemo(() => channels.map((channel) => channel.elId), [channels]);
   const [activeCategory, setActiveCategory] = useState<ICategory | null>(null);
   const [activeChannel, setActiveChannel] = useState<IChannel | null>(null);
 
@@ -151,135 +148,125 @@ export const CommunityItems = ({ className, setSlideIndex }: ICommunityItems) =>
       activationConstraint: {
         distance: 10,
       },
-    })
+    }),
   );
 
   return (
-    <DndContext measuring={{
-      droppable: {
-        strategy: MeasuringStrategy.Always,
-      },
-    }}
-        autoScroll={false}
-        collisionDetection={closestCorners} 
-        sensors={sensors}
-        onDragStart={onDragStart}
-        onDragEnd={onDragEnd}
-        onDragOver={onDragOver}
-      >
+    <DndContext
+      measuring={{
+        droppable: {
+          strategy: MeasuringStrategy.Always,
+        },
+      }}
+      autoScroll={false}
+      collisionDetection={closestCorners}
+      sensors={sensors}
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+      onDragOver={onDragOver}
+    >
       <ul
         className={`mt-3 flex h-[calc(100dvh-128px)] flex-col gap-1 overflow-y-auto px-5 sm:mb-[60px] sm:px-1 ${
           className ?? ''
         }`}
       >
         <SortableContext strategy={verticalListSortingStrategy} items={categoryIds}>
-              {categories.map((cat) => (
-                <div className='my-2'>
-                  <CategoryContainer setSlideIndex={setSlideIndex}
-                  key={cat.id}
-                  category={cat}
-                  channels={channels.filter((channel) => channel.categoryId === cat.id)}
-                  />
-                </div>
-              ))}
+          {categories.map((cat) => (
+            <div key={cat.id} className="my-2">
+              <CategoryContainer
+                setSlideIndex={setSlideIndex}
+                category={cat}
+                channels={channels.filter((channel) => channel.categoryId === cat.id)}
+              />
+            </div>
+          ))}
         </SortableContext>
       </ul>
       {createPortal(
-          <DragOverlay>
-            {activeCategory && (
-                <CategoryContainer
-                category={activeCategory}
-                channels={channels.filter(
-                  (channel) => channel.categoryId === activeCategory.id
-                )}
-              />
-            )}
-            {activeChannel && (
-              <SortableContext strategy={verticalListSortingStrategy} items={channelsIds}>
-              <Channel
-                channel={activeChannel}
-              />
-              </SortableContext>
-            )}
-          </DragOverlay>,
-          document.body
-        )}
+        <DragOverlay>
+          {activeCategory && (
+            <CategoryContainer
+              category={activeCategory}
+              channels={channels.filter((channel) => channel.categoryId === activeCategory.id)}
+            />
+          )}
+          {activeChannel && (
+            <SortableContext strategy={verticalListSortingStrategy} items={channelsIds}>
+              <Channel channel={activeChannel} />
+            </SortableContext>
+          )}
+        </DragOverlay>,
+        document.body,
+      )}
     </DndContext>
   );
 
-  function onDragStart(event: DragStartEvent) {
-    if (event.active.data.current?.type === "Category") {
-      setActiveCategory(event.active.data.current.category);
-      return;
-    }
-    
-    if (event.active.data.current?.type === "Channel") {
-      setActiveChannel(event.active.data.current.channel);
-      return;
+  function onDragStart(event: DragStartEvent): void {
+    const currentType = event.active.data.current?.type;
+    if (currentType === 'Category') {
+      setActiveCategory(event.active.data.current?.category);
+    } else if (currentType === 'Channel') {
+      setActiveChannel(event.active.data.current?.channel);
     }
   }
-  
-  
-  function onDragEnd(event: DragEndEvent) {
+
+  function onDragEnd(event: DragEndEvent): void {
     setActiveCategory(null);
     setActiveChannel(null);
-  
+
     const { active, over } = event;
     if (!over) return;
-  
+
     const activeId = active.id;
     const overId = over.id;
-  
+
     if (activeId === overId) return;
-  
-    const isActiveACategory = active.data.current?.type === "Category";
+
+    const isActiveACategory = active.data.current?.type === 'Category';
     if (!isActiveACategory) return;
 
-  
-    setCategories((category) => {
+    setCategories((categories) => {
       const activeCategoryIndex = categories.findIndex((cat) => cat.id === activeId);
-  
       const overCategoryIndex = categories.findIndex((cat) => cat.id === overId);
-  
-      return arrayMove(category, activeCategoryIndex, overCategoryIndex);
+      return arrayMove(categories, activeCategoryIndex, overCategoryIndex);
     });
   }
-  
-  
-  function onDragOver(event: DragOverEvent) {
+
+  function onDragOver(event: DragOverEvent): void {
     const { active, over } = event;
     if (!over) return;
-  
-    const activeId = active?.id;
-    const overId = over?.id;
-  
+
+    const activeId = active?.id as string;
+    const overId = over?.id as string;
+
     if (activeId === overId) return;
-  
-    const isActiveAChannel = active.data.current?.type === "Channel";
-    const isOverAChannel = over.data.current?.type === "Channel";
+
+    const isActiveAChannel = active.data.current?.type === ('Channel' as string);
+    const isOverAChannel = over.data.current?.type === ('Channel' as string);
 
     if (!isActiveAChannel) return;
 
     if (isActiveAChannel && isOverAChannel) {
-      setChannels((channels) => {
-        const activeIndex = channels.findIndex((channel) => channel.elId === activeId);
-        const overIndex = channels.findIndex((channel) => channel.elId === overId);
-  
-        if (channels[activeIndex].categoryId != channels[overIndex].categoryId) {
+      setChannels((channels: IChannel[]) => {
+        const activeIndex = channels.findIndex(
+          (channel: IChannel) => channel.elId === activeId,
+        ) as number;
+        const overIndex = channels.findIndex(
+          (channel: IChannel) => channel.elId === overId,
+        ) as number;
+
+        if (channels[activeIndex].categoryId !== channels[overIndex].categoryId) {
           channels[activeIndex].categoryId = channels[overIndex].categoryId;
-          return arrayMove(channels, activeIndex, 0);
-          // overIndex - 1
+          return arrayMove(channels, activeIndex, overIndex);
         }
-  
         return arrayMove(channels, activeIndex, overIndex);
       });
     } else if (isActiveAChannel && !isOverAChannel) {
-      setChannels((channels) => {
-        const activeIndex = channels.findIndex((channel) => channel.elId === activeId);
-        channels[activeIndex].categoryId = String(overId);
+      setChannels((channels: IChannel[]) => {
+        const activeIndex = channels.findIndex((channel: IChannel) => channel.elId === activeId);
+        channels[activeIndex].categoryId = overId;
         return arrayMove(channels, activeIndex, 0);
       });
     }
   }
-}
-
+};
