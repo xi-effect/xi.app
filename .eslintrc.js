a0d68910a09a9985c2f1aa382370242c6207baf8
unknownproperty <igor.buksheff@yandex.ru>
2024-04-26 16:57:48 +0300

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,10 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    root: true,
+    // This tells ESLint to load the config from the package `eslint-config-custom`
+    extends: ['custom'],
+    settings: {
+        next: {
+            rootDir: ['apps/*/'],
+        },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+};
