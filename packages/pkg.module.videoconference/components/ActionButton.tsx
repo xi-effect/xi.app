e5067c5497df4d4a4d611b94ad49cdf93c7f7456
Ley official <danyilfreelance@gmail.com>
2024-05-29 12:06:12 +0100

    fix 34092843 : fixed a border line

--- packages/pkg.module.videoconference/components/ActionButton.tsx
+++ packages/pkg.module.videoconference/components/ActionButton.tsx
@@ -10,7 +10,7 @@ export interface IActionButton {
 export function ActionButton({ icon, enable, withBorder = true }: IActionButton) {
   return (
     <div
-      className={`border-4 ${enable && withBorder ? 'border-green-60' : 'border-red-60'} ${!withBorder ? 'border-none' : null} ml-0.5 flex h-12 w-12 cursor-pointer flex-row items-center justify-center rounded-[24px] bg-gray-100`}
+      className={`border-2 ${enable && withBorder ? 'border-green-60' : 'border-red-60'} ${!withBorder ? 'border-none' : null} ml-0.5 flex h-12 w-12 cursor-pointer flex-row items-center justify-center rounded-[24px] bg-gray-100`}
     >
       {icon}
     </div>
