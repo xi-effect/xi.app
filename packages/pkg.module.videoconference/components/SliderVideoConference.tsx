e885fbc343a650bd3808534b758d8217f4e2b519
Ley official <danyilfreelance@gmail.com>
2024-06-04 18:17:56 +0100

    feat 34389769: added a trackLoop render

--- packages/pkg.module.videoconference/components/SliderVideoConference.tsx
+++ packages/pkg.module.videoconference/components/SliderVideoConference.tsx
@@ -1,32 +1,80 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './Slider.css';
+import { getTrackReferenceId } from '@livekit/components-core';
+import { TrackLoopProps, TrackRefContext } from '@livekit/components-react';
 
-export function SliderVideoConference() {
+interface ITrackLoopProps {
+  maxVisibleTiles: number;
+}
+
+export function SliderVideoConference({
+  tracks,
+  maxVisibleTiles,
+  ...props
+}: TrackLoopProps & ITrackLoopProps) {
   const settings = {
     dots: true,
-    slidesToShow: 2,
     infinite: false,
+    speed: 500,
+    slidesToShow: maxVisibleTiles,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+  function cloneSingleChild(
+    children: React.ReactNode | React.ReactNode[],
+    props?: Record<string, any>,
+    key?: any,
+  ) {
+    return React.Children.map(children, (child) => {
+      // Checking isValidElement is the safe way and avoids a typescript
+      // error too.
+      if (React.isValidElement(child) && React.Children.only(children)) {
+        return React.cloneElement(child, { ...props, key });
+      }
+      return child;
+    });
+  }
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        <div>
-          <h2>Hello 1</h2>
-        </div>
-        <div>
-          <h2>Hello 2</h2>
-        </div>
-        <div>
-          <h2>Hello 3</h2>
-        </div>
-        <div>
-          <h2>Hello 4</h2>
-        </div>
+        {tracks.map((trackReference: any) => (
+          <TrackRefContext.Provider
+            value={trackReference}
+            key={getTrackReferenceId(trackReference)}
+          >
+            {cloneSingleChild(props.children)}
+          </TrackRefContext.Provider>
+        ))}
       </Slider>
     </div>
   );
