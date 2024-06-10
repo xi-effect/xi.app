493e2d5215e6b3ee729570802cce1cb08634fcba
Ley official <danyilfreelance@gmail.com>
2024-06-10 13:17:47 +0100

    fix 34389769: deleted slick liblary

--- packages/pkg.module.videoconference/components/SliderVideoConference.tsx
+++ packages/pkg.module.videoconference/components/SliderVideoConference.tsx
@@ -1,13 +1,9 @@
 /* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import './Slider.css';
 import { getTrackReferenceId } from '@livekit/components-core';
 import { TrackLoopProps, TrackRefContext } from '@livekit/components-react';
-import { ChevronLeft, ChevronRight } from '@xipkg/icons';
+// import { ChevronLeft, ChevronRight } from '@xipkg/icons';
 import { IOrientationLayout } from './VideoConferenceLayout';
 
 interface ITrackLoopProps {
@@ -16,21 +12,10 @@ interface ITrackLoopProps {
 
 export function SliderVideoConference({
   tracks,
-  maxVisibleTiles,
-  orientation,
+  // maxVisibleTiles,
+  // orientation,
   ...props
 }: TrackLoopProps & ITrackLoopProps & IOrientationLayout) {
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 2,
-    vertical: orientation === 'vertical',
-    verticalSwiping: orientation === 'vertical',
-    nextArrow: <ChevronRight />,
-    prevArrow: <ChevronLeft />,
-    slidesToScroll: maxVisibleTiles,
-  };
   function cloneSingleChild(
     children: React.ReactNode | React.ReactNode[],
     props?: Record<string, any>,
@@ -45,18 +30,15 @@ export function SliderVideoConference({
   }
   return (
     <div className="slider-container">
-      {tracks.length > 0 && (
-        <Slider {...settings}>
-          {tracks.map((trackReference: any) => (
-            <TrackRefContext.Provider
-              value={trackReference}
-              key={getTrackReferenceId(trackReference)}
-            >
-              {cloneSingleChild(props.children)}
-            </TrackRefContext.Provider>
-          ))}
-        </Slider>
-      )}
+      {tracks.length > 0 &&
+        tracks.map((trackReference: any) => (
+          <TrackRefContext.Provider
+            value={trackReference}
+            key={getTrackReferenceId(trackReference)}
+          >
+            {cloneSingleChild(props.children)}
+          </TrackRefContext.Provider>
+        ))}
     </div>
   );
 }
