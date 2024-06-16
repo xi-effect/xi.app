c90a04aca33b1154f02d5a5c6c2d0e4862fbc0df
Ley official <danyilfreelance@gmail.com>
2024-06-16 18:19:20 +0100

    fix 34389769: fixed a btn of changecarousel

--- packages/pkg.module.videoconference/components/Carousel.tsx
+++ packages/pkg.module.videoconference/components/Carousel.tsx
@@ -18,19 +18,21 @@ export const Carousel = ({
   <div className="mx-auto h-full w-full">
     <div className="relative h-full overflow-hidden">
       <div
-        className={`absolute flex h-full w-full items-center justify-between ${orientation === 'horizontal' ? 'flex-row' : 'flex-col'}`}
+        className={`absolute inset-0 flex items-center justify-between ${orientation === 'horizontal' ? 'flex-row' : 'flex-col'}`}
       >
         <button
           type="button"
           disabled={handleCheckDisabled('prev')}
           onClick={handlePrev}
-          className="disabled:fill-gray-80 z-10 bg-transparent fill-white p-0 text-center transition-all duration-300 ease-in-out hover:opacity-100 disabled:cursor-not-allowed"
+          className="disabled:fill-gray-80 z-10 bg-transparent fill-white p-0 text-center hover:opacity-100 disabled:cursor-not-allowed"
         >
-          {orientation === 'horizontal' ? (
-            <ChevronLeft className="fill-inherit" />
-          ) : (
-            <ChevronLeft className="rotate-90 fill-inherit" />
-          )}
+          <div className="flex items-center justify-center rounded-full bg-[#000000] p-2">
+            {orientation === 'horizontal' ? (
+              <ChevronLeft className="fill-inherit" />
+            ) : (
+              <ChevronLeft className="rotate-90 fill-inherit" />
+            )}
+          </div>
           <span className="sr-only">Prev</span>
         </button>
         <button
@@ -39,16 +41,18 @@ export const Carousel = ({
           onClick={handleNext}
           className="disabled:fill-gray-80 z-10 bg-transparent fill-white p-0 text-center hover:opacity-100 disabled:cursor-not-allowed"
         >
-          {orientation === 'horizontal' ? (
-            <ChevronRight className="fill-inherit" />
-          ) : (
-            <ChevronRight className="rotate-90 fill-inherit" />
-          )}
+          <div className="flex items-center justify-center rounded-full bg-[#000000] p-2">
+            {orientation === 'horizontal' ? (
+              <ChevronRight className="fill-inherit" />
+            ) : (
+              <ChevronRight className="rotate-90 fill-inherit" />
+            )}
+          </div>
           <span className="sr-only">Next</span>
         </button>
       </div>
       <div
-        className={`${orientation === 'vertical' ? 'my-10 flex h-[calc(100vh-20rem)] flex-col' : 'mx-10'} carousel-container relative z-0 flex h-full w-full touch-pan-x snap-x snap-mandatory gap-5 overflow-hidden scroll-smooth`}
+        className={`${orientation === 'vertical' ? 'my-12 flex h-[calc(100vh-20rem)] flex-col' : 'mx-12'} relative z-0 flex h-full w-full touch-pan-x snap-x snap-mandatory gap-5 overflow-hidden scroll-smooth`}
       >
         {children}
       </div>
