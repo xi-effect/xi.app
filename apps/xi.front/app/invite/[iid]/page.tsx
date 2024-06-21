f088b34be5b7123584b9d6382d46e7209842bf21
d9m0n4 <c2h8e9d4@gmail.com>
2024-06-21 07:44:22 +0300

    fix(34683536): fix unused types

--- apps/xi.front/app/invite/[iid]/page.tsx
+++ apps/xi.front/app/invite/[iid]/page.tsx
@@ -3,25 +3,23 @@ import { Metadata } from 'next';
 // import { get } from 'pkg.utils';
 import InvitePage from './invitePage';
 
-type ResponseBodyT = {
-  community: {
-    id: number;
-    name: string;
-    description: string;
-  };
-  is_authorized: boolean;
-  has_already_joined: boolean;
-};
+// type ResponseBodyT = {
+//   community: {
+//     id: number;
+//     name: string;
+//     description: string;
+//   };
+//   is_authorized: boolean;
+//   has_already_joined: boolean;
+// };
 
 type PageParamsT = {
   params: {
-    iid: string
-  }
+    iid: string;
+  };
 };
 
-export async function generateMetadata(
-    { params }: PageParamsT,
-): Promise<Metadata> {
+export async function generateMetadata({ params }: PageParamsT): Promise<Metadata> {
   // const { status, datas } = await get<ResponseBodyT>({
   //   service: 'backend',
   //   path: `/api/public/community-service/invitations/by-code/${params.iid}/community/`,
@@ -35,8 +33,8 @@ export async function generateMetadata(
   // если использю get из pkg.utils то появляется ошибка:
   // импорт компонентов без пометки 'use client'
   const data = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_URL_BACKEND}/api/public/community-service/invitations/by-code/${params.iid}/community/`)
-      .then((res) => res.json());
+    `${process.env.NEXT_PUBLIC_SERVER_URL_BACKEND}/api/public/community-service/invitations/by-code/${params.iid}/community/`,
+  ).then((res) => res.json());
   return {
     title: `Приглашение в сообщество ${data.community.name}`,
     description: data.community.description || 'Добро пожаловать в лучшее сообщество',
@@ -48,8 +46,6 @@ export async function generateMetadata(
   };
 }
 
-const Invite = ({ params }: PageParamsT) => (
-  <InvitePage params={params} />
-  );
+const Invite = ({ params }: PageParamsT) => <InvitePage params={params} />;
 
 export default Invite;
