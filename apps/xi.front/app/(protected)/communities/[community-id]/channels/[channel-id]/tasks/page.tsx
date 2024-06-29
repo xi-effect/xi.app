81c07fb6af676b5b2a66426579fdf7b13cd3b26c
liluser <email@example.com>
2024-06-30 00:08:13 +0300

    feat(34610334)

--- apps/xi.front/app/(protected)/communities/[community-id]/channels/[channel-id]/tasks/page.tsx
+++ apps/xi.front/app/(protected)/communities/[community-id]/channels/[channel-id]/tasks/page.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+'use client';
 
-export default function Tasks() {
-  return (
-    <div className="w-full md:w-[calc(100vw-350px)] md:min-h-screen md:h-screen md:overflow-auto bg-gray-0">
-      tasks
-    </div>
-  );
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+const Tasks = dynamic(() => import('pkg.module.tasks').then((mod) => mod.Tasks));
+
+export default function TasksPage() {
+  return <Tasks />;
 }
