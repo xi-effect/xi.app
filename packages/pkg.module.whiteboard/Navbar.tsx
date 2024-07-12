cabe0a57283f5fb1bb33b48949a2106ec26c6bdd
Ley official <danyilfreelance@gmail.com>
2024-07-12 14:03:09 +0100

    feat 35672435: added a track to make our component reactive

--- packages/pkg.module.whiteboard/Navbar.tsx
+++ packages/pkg.module.whiteboard/Navbar.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { useEditor } from 'tldraw';
+import React, { useEffect } from 'react';
+import { track, useEditor } from 'tldraw';
 // import './custom-ui.css';
 
-export function Navbar() {
+export const Navbar = track(() => {
   const editor = useEditor();
-  const [activeBtn, setActiveBtn] = React.useState('select');
   React.useEffect(() => {
     const handleKeyUp = (e: KeyboardEvent) => {
       switch (e.key) {
@@ -52,17 +51,14 @@ export function Navbar() {
   return (
     <div className="pointer-events-none absolute inset-0">
       <div className="absolute bottom-3 left-0 right-0 flex w-full items-center justify-center">
-        <div className="z-[300] flex gap-10 bg-white">
-          <div className="border-gray-10 z-[300] flex gap-2 rounded-[12px] border p-1">
+        <div className="border-gray-10 z-[300] flex gap-10 rounded-[12px] border bg-white">
+          <div className="flex gap-2 p-1">
             {navBarElements.map((item) => (
               <button
                 key={item.action}
-                className={`pointer-events-auto h-[32px] w-[32px] items-center rounded-[8px] ${activeBtn == item.action ? 'bg-brand-0' : 'bg-white'}`}
+                className={`pointer-events-auto h-[32px] w-[32px] items-center rounded-[8px] ${editor.getCurrentToolId() == item.action ? 'bg-brand-0' : 'bg-white'}`}
                 data-isactive={editor.getCurrentToolId() === item.action}
-                onClick={() => {
-                  setActiveBtn(item.action);
-                  editor.setCurrentTool(item.action);
-                }}
+                onClick={() => editor.setCurrentTool(item.action)}
               >
                 {item.title}
               </button>
@@ -72,4 +68,4 @@ export function Navbar() {
       </div>
     </div>
   );
-}
+});
