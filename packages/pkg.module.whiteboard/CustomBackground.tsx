846c84487ffd7a93fcd0406742a8e4cb1e2c4e32
Ley official <danyilfreelance@gmail.com>
2024-07-12 01:39:51 +0100

    feat 35672435: added custom zoom menu

--- packages/pkg.module.whiteboard/CustomBackground.tsx
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import { DefaultGrid } from './DefaultGrid';
-
-export function CustomBackground() {
-  return <DefaultGrid x={0} y={0} z={1} size={10} />;
-}
--- packages/pkg.module.whiteboard/WhiteBoard.tsx
+++ packages/pkg.module.whiteboard/WhiteBoard.tsx
@@ -1,26 +1,19 @@
 'use client';
 import React from 'react';
-import {
-  DefaultBackground,
-  DefaultGrid,
-  DefaultMainMenu,
-  Tldraw,
-  TldrawOptions,
-  ToggleGridItem,
-} from 'tldraw';
+import { Tldraw } from 'tldraw';
 import { Navbar } from './Navbar';
-import { CustomBackground } from './CustomBackground';
 import './index.css';
 import { CustomZoomMenu } from './CustomZoomMenu';
 
 export const WhiteBoard: React.FC = () => {
   return (
-    <Tldraw hideUi>
+    <Tldraw
+      onMount={(editor) => {
+        editor.updateInstanceState({ isGridMode: true });
+      }}
+      hideUi
+    >
       <Navbar />
-      <div className="absolute z-[300]">
-        <DefaultMainMenu />
-      </div>
-      <CustomBackground />
       <CustomZoomMenu />
     </Tldraw>
   );
