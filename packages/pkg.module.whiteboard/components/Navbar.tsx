e1e9da7c06f3e7906a68e9c0037d41c95955e519
Ley official <danyilfreelance@gmail.com>
2024-07-21 11:50:21 +0100

    feat 35672435: added icons to the navbar

--- packages/pkg.module.whiteboard/components/Navbar.tsx
+++ packages/pkg.module.whiteboard/components/Navbar.tsx
@@ -1,19 +1,26 @@
 import { track, useEditor } from 'tldraw';
 import { NavbarAction } from './NavbarAction';
+import { Arrow, Cursor, Eraser, Figures, Hand, Image, Pen, Sticker, TText } from '@xipkg/icons';
+
+type TNavbarElement = {
+  action: string;
+  title: string;
+  icon: JSX.Element | null;
+};
 
 export const Navbar = track(() => {
   const editor = useEditor();
 
-  const navBarElements = [
-    { action: 'select', title: 'Select' },
-    { action: 'hand', title: 'Hand' },
-    { action: 'draw', title: 'Draw' },
-    { action: 'note', title: 'Sticker' },
-    { action: 'text', title: 'Text' },
-    { action: 'rectangle', title: 'Shapes' },
-    { action: 'arrow', title: 'Arrow' },
-    { action: 'image', title: 'Image' },
-    { action: 'eraser', title: 'Eraser' },
+  const navBarElements: TNavbarElement[] = [
+    { action: 'select', title: 'Select', icon: <Cursor /> },
+    { action: 'hand', title: 'Hand', icon: <Hand /> },
+    { action: 'draw', title: 'Draw', icon: <Pen /> },
+    { action: 'note', title: 'Sticker', icon: <Sticker /> },
+    { action: 'text', title: 'Text', icon: <TText /> },
+    { action: 'rectangle', title: 'Shapes', icon: <Figures /> },
+    { action: 'arrow', title: 'Arrow', icon: <Arrow /> },
+    { action: 'image', title: 'Image', icon: <Image /> },
+    { action: 'eraser', title: 'Eraser', icon: <Eraser /> },
   ];
 
   return (
@@ -25,14 +32,14 @@ export const Navbar = track(() => {
           </div>
           <div className="border-gray-10 z-[300] flex gap-10 rounded-[12px] border bg-white">
             <div className="flex gap-2 p-1">
-              {navBarElements.map((item) => (
+              {navBarElements.map((item: TNavbarElement) => (
                 <button
                   key={item.action}
-                  className={`pointer-events-auto h-[32px] w-[32px] items-center rounded-[8px] ${editor.getCurrentToolId() == item.action ? 'bg-brand-0' : 'bg-white'}`}
+                  className={`pointer-events-auto flex h-[32px] w-[32px] items-center justify-center rounded-[8px] ${editor.getCurrentToolId() == item.action ? 'bg-brand-0' : 'bg-white'}`}
                   data-isactive={editor.getCurrentToolId() === item.action}
                   onClick={() => editor.setCurrentTool(item.action)}
                 >
-                  {item.title}
+                  {item.icon ? item.icon : item.title}
                 </button>
               ))}
             </div>
