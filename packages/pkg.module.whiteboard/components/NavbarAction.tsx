780e07da6c2201b901255cdcc83278b0fb888412
Ley official <danyilfreelance@gmail.com>
2024-07-21 12:59:56 +0100

    feat 35672435: small refactor of component NavbarAction

--- packages/pkg.module.whiteboard/components/NavbarAction.tsx
+++ packages/pkg.module.whiteboard/components/NavbarAction.tsx
@@ -6,19 +6,12 @@ export function NavbarAction() {
   const canRedo = useCanRedo();
   const canUndo = useCanUndo();
 
-  const handleUndo = () => {
-    editor.undo();
-  };
-  const handleRedo = () => {
-    editor.redo();
-  };
-
   return (
     <div className="pointer-events-auto flex items-center justify-center gap-5">
-      <button className="items-center rounded-[8px] bg-white" onClick={handleUndo}>
+      <button className="items-center rounded-[8px] bg-white" onClick={() => editor.undo()}>
         <Undo className={`${canUndo ? null : 'fill-gray-40'}`} />
       </button>
-      <button className="items-center rounded-[8px] bg-white" onClick={handleRedo}>
+      <button className="items-center rounded-[8px] bg-white" onClick={() => editor.redo()}>
         <Redo className={`${canRedo ? null : 'fill-gray-40'}`} />
       </button>
     </div>
