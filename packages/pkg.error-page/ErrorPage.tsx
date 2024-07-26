b21dc866549c0b5a1aac615e5cdbc30962ed3df0
Julia Bednaia <juliabednaia@gmail.com>
2024-07-26 10:02:25 +0300

    fix(34683590-fix): replaced font sizes with config values

--- packages/pkg.error-page/ErrorPage.tsx
+++ packages/pkg.error-page/ErrorPage.tsx
@@ -32,15 +32,15 @@ export const ErrorPage = ({ title, errorCode, text, children }: ErrorPagePropsT)
     </div>
     <div className="flex flex-col justify-center">
       <span className="flex flex-col-reverse sm:flex-row sm:gap-1">
-        <h1 className="text-gray-90 mb-4 text-[40px] font-bold leading-[48px] sm:text-[48px] sm:leading=[58px] xl:text-[64px] xl:leading-[78px]">
+        <h1 className="text-gray-90 mb-4 text-h3 font-bold sm:text-h2 xl:text-[64px] xl:leading-[78px]">
           {title}
         </h1>
-        <span className="text-gray-30 text-[28px] font-bold xl:text-[40px]">{errorCode}</span>
+        <span className="text-gray-30 text-h6 font-bold xl:text-h3">{errorCode}</span>
       </span>
-      <p className="text-gray-90 mb-16 text-[20px] font-normal sm:text-[24px] xl:text-[30px]">
+      <p className="text-gray-90 mb-16 text-l-base font-normal sm:text-xl-base xl:text-[30px]">
         {text}
       </p>
-      <p className="text-gray-80 text-[16px]">
+      <p className="text-gray-80 text-m-base">
         Если ошибка повторяется — напишите <span className="hidden sm:inline">нам об этом</span>
         <span className="flex flex-col sm:flex-row">
           <span className="flex items-center">
@@ -59,7 +59,7 @@ export const ErrorPage = ({ title, errorCode, text, children }: ErrorPagePropsT)
           </span>
         </span>
       </p>
-      <div className="text-gray-80 mt-[64px] text-[16px]">{children}</div>
+      <div className="text-gray-80 mt-[64px] text-m-base">{children}</div>
     </div>
     <div className="min-h-[44px] h-[88px] xl:h-[132px] xl:min-h-[52px]" />
   </main>
