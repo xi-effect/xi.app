e50d7855c022700e48c321cac5d785ce72e85ae9
Temich21 <rahmat97@mail.ru>
2024-08-16 15:04:34 +0200

    Fix(37010033): Collisions fix

--- apps/xi.front/app/(protected)/communities/[community-id]/channels/[channel-id]/editor/page.tsx
+++ apps/xi.front/app/(protected)/communities/[community-id]/channels/[channel-id]/editor/page.tsx
@@ -9,8 +9,8 @@ const Editor = dynamic(() => import('pkg.module.editor').then((mod) => mod.Edito
 export default function EditorPage() {
   return (
     <div className="p-4 w-full md:w-[calc(100vw-350px)] md:min-h-screen md:h-screen md:overflow-auto bg-gray-0">
-      <Editor initialValue={mockValues} />
-      {/* <Editor initialValue={mockValues} readOnly /> */}
+      {/* <Editor initialValue={mockValues} /> */}
+      <Editor initialValue={mockValues} readOnly />
     </div>
   );
 }
