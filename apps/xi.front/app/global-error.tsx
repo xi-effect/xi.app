f0db3378665515be036180466f621230ed166337
unknownproperty <igor.buksheff@yandex.ru>
2024-08-18 19:32:27 +0300

    feat: try to add Sentry

--- apps/xi.front/app/global-error.tsx
+++ apps/xi.front/app/global-error.tsx
@@ -1,8 +1,15 @@
 'use client';
 
 import { ErrorPage } from 'pkg.error-page';
+import { useEffect } from 'react';
+import * as Sentry from '@sentry/nextjs';
+import Error from 'next/error';
 
-export default function Error500() {
+export default function Error500({ error }: { error: Error }) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
   return (
     <ErrorPage
       title="Сервис недоступен"
