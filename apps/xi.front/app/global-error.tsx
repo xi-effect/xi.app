68ff8243c657a6e03a3242551137778059b78cef
unknownproperty <igor.buksheff@yandex.ru>
2024-08-18 19:47:33 +0300

    Revert "feat: try to add Sentry"
    This reverts commit f0db3378665515be036180466f621230ed166337.

--- apps/xi.front/app/global-error.tsx
+++ apps/xi.front/app/global-error.tsx
@@ -1,15 +1,8 @@
 'use client';
 
 import { ErrorPage } from 'pkg.error-page';
-import { useEffect } from 'react';
-import * as Sentry from '@sentry/nextjs';
-import Error from 'next/error';
 
-export default function Error500({ error }: { error: Error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
+export default function Error500() {
   return (
     <ErrorPage
       title="Сервис недоступен"
--- apps/xi.front/next.config.js
+++ apps/xi.front/next.config.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-extra-boolean-cast */
 // Не поддавайтесь соблазну использовать здесь import
 const path = require('path');
 const runtimeCaching = require('next-pwa/cache');
-const { withSentryConfig } = require('@sentry/nextjs');
 
 const plugins = [];
 
@@ -101,54 +99,5 @@ const nextConfig = {
     return config;
   },
 };
-
-if (!!process.env.NEXT_PUBLIC_SENTRY_DSN) {
-  plugins.push((nextConfig) =>
-    withSentryConfig(nextConfig, {
-      // For all available options, see:
-      // https://github.com/getsentry/sentry-webpack-plugin#options
-
-      org: 'xieffect',
-      project: 'xi-app',
-      sentryUrl: 'https://xieffect.sentry.io/',
-
-      // Only print logs for uploading source maps in CI
-      silent: !process.env.CI,
-
-      // For all available options, see:
-      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
-
-      // Automatically annotate React components
-      // to show their full name in breadcrumbs and session replay
-      reactComponentAnnotation: {
-        enabled: true,
-      },
-
-      // Uncomment to route browser requests
-      // to Sentry through a Next.js rewrite to circumvent ad-blockers.
-      // This can increase your server load as well as your hosting bill.
-      // Note: Check that the configured route
-      // will not match with your Next.js middleware, otherwise reporting of client-
-      // side errors will fail.
-      // tunnelRoute: "/monitoring",
-
-      // Hides source maps from generated client bundles
-      hideSourceMaps: true,
-
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      disableLogger: true,
-
-      // Enables automatic instrumentation of Vercel Cron Monitors.
-      // (Does not yet work with App Router route handlers.)
-      // See the following for more information:
-      // https://docs.sentry.io/product/crons/
-      // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: true,
-    }),
-  );
-}
 
 module.exports = () => plugins.reduce((acc, next) => next(acc), nextConfig);
