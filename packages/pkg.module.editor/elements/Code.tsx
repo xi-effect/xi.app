59738c90b307edecae643e74021ba2d64a45cf11
d9m0n4 <ched08@yandex.ru>
2024-08-19 23:02:49 +0300

    fix(37010102): add code font style

--- packages/pkg.module.editor/elements/Code.tsx
+++ packages/pkg.module.editor/elements/Code.tsx
@@ -1,8 +1,18 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import { IBM_Plex_Mono } from 'next/font/google';
 import { type CustomRenderElementProps } from './RenderElement';
 import { CustomText } from '../slate';
 
 type CodePropsT = CustomRenderElementProps;
+
+const codeFont = IBM_Plex_Mono({
+  weight: ['400', '500', '600'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-ibm-plex-mono',
+});
 
 export const Code = ({ element, children, attributes }: CodePropsT) => {
   const isEmpty =
@@ -11,8 +21,8 @@ export const Code = ({ element, children, attributes }: CodePropsT) => {
     element.children.length === 1;
 
   return (
-    <pre
-      className="border-gray-10 block w-full whitespace-pre-wrap break-words rounded-lg border p-4 font-mono text-sm"
+    <div
+      className={`${codeFont.className} text-s-base border-gray-10 text-sm" block w-full whitespace-pre-wrap break-words rounded-lg border p-4`}
       {...attributes}
     >
       {isEmpty && (
@@ -20,7 +30,7 @@ export const Code = ({ element, children, attributes }: CodePropsT) => {
           Введите фрагмент кода
         </span>
       )}
-      <code>{children}</code>
-    </pre>
+      {children}
+    </div>
   );
 };
