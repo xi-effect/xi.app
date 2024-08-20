7cd9982c1b180008f8bb99917da3226315b7975a
Ley official <danyilfreelance@gmail.com>
2024-08-20 13:11:43 +0100

    feat 37155924: added change opacity and size of the shape on the board

--- packages/pkg.module.whiteboard/components/StyleMenu.tsx
+++ packages/pkg.module.whiteboard/components/StyleMenu.tsx
@@ -2,6 +2,7 @@ import './styleMenu.css';
 
 import {
   CommonStylePickerSet,
+  DefaultSizeStyle,
   getDefaultColorTheme,
   useEditor,
   useIsDarkMode,
@@ -17,12 +18,36 @@ export const StyleMenu = () => {
   if (!styles) return null;
   const theme = getDefaultColorTheme({ isDarkMode: isDarkMode });
 
+  const sizes = ['s', 'm', 'l', 'xl'] as const;
+
+  const handleSize = (value: number[]) => {
+    const size = sizes[value[0] - 1];
+    editor.setStyleForNextShapes(DefaultSizeStyle, size);
+  };
+
+  const handleOpacity = (value: number[]) => {
+    editor.setOpacityForNextShapes(value[0] / 100);
+  };
+
   return (
     <div className="border-gray-10 bg-gray-0 w-full">
       <div className="p-6">
         <div className="flex flex-col gap-8">
-          <Slider defaultValue={[1]} max={6} step={1} minStepsBetweenThumbs={1} />
-          <Slider defaultValue={[100]} max={100} step={1} />
+          <Slider
+            onValueChange={(value) => handleSize(value)}
+            defaultValue={[2]}
+            min={1}
+            max={4}
+            step={1}
+            minStepsBetweenThumbs={1}
+          />
+          <Slider
+            onValueChange={(value) => handleOpacity(value)}
+            defaultValue={[100]}
+            min={10}
+            max={100}
+            step={1}
+          />
         </div>
       </div>
       <div className="border-gray-10 border-t pb-4"></div>
