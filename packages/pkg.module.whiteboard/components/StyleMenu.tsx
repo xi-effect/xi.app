5804c76d445ea6c6a0d45521c8e64ef6964f0f72
Ley official <danyilfreelance@gmail.com>
2024-08-26 18:11:16 +0100

    fix 37155924: fixed grid in colors and added measures for sliders

--- packages/pkg.module.whiteboard/components/StyleMenu.tsx
+++ packages/pkg.module.whiteboard/components/StyleMenu.tsx
@@ -8,6 +8,7 @@ import {
   useRelevantStyles,
 } from 'tldraw';
 import { Slider } from '@xipkg/slider';
+import { useState } from 'react';
 
 const sizes = ['s', 'm', 'l', 'xl'] as const;
 
@@ -16,41 +17,63 @@ export const StyleMenu = () => {
   const isDarkMode = useIsDarkMode();
   const styles = useRelevantStyles();
 
+  const [currentSize, setCurrentSize] = useState<string>('m');
+  const [currentOpacity, setCurrentOpacity] = useState<number>(100);
+
   if (!styles) return null;
   const theme = getDefaultColorTheme({ isDarkMode: isDarkMode });
 
   const handleSize = (value: number[]) => {
     const size = sizes[value[0] - 1];
+    setCurrentSize(size);
     editor.setStyleForNextShapes(DefaultSizeStyle, size);
   };
 
   const handleOpacity = (value: number[]) => {
-    editor.setOpacityForNextShapes(value[0] / 100);
+    const opacity = value[0] / 100;
+    editor.setOpacityForNextShapes(opacity);
+    setCurrentOpacity(opacity);
   };
 
   return (
     <div className="border-gray-10 bg-gray-0 w-full">
       <div className="p-6">
         <div className="flex flex-col gap-8">
-          <Slider
-            onValueChange={(value) => handleSize(value)}
-            defaultValue={[2]}
-            min={1}
-            max={4}
-            step={1}
-            minStepsBetweenThumbs={1}
-          />
-          <Slider
-            onValueChange={(value) => handleOpacity(value)}
-            defaultValue={[100]}
-            min={10}
-            max={100}
-            step={1}
-          />
+          <div className="flex items-center justify-between gap-4">
+            <div className="w-full">
+              <Slider
+                onValueChange={(value) => handleSize(value)}
+                defaultValue={[2]}
+                min={1}
+                max={4}
+                step={1}
+                minStepsBetweenThumbs={1}
+              />
+            </div>
+            <div className="w-4">
+              <p>{currentSize.toUpperCase()}</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-between gap-4">
+            <div className="w-full">
+              <Slider
+                onValueChange={(value) => handleOpacity(value)}
+                defaultValue={[100]}
+                min={10}
+                max={100}
+                step={1}
+              />
+            </div>
+            <div className="w-4">
+              <p>{currentOpacity}</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="border-gray-10 border-t pb-4"></div>
-      <CommonStylePickerSet theme={theme} styles={styles} />
+      <div className="border-gray-10 border-t"></div>
+      <div className="px-4 py-2">
+        <CommonStylePickerSet theme={theme} styles={styles} />
+      </div>
     </div>
   );
 };
