18a8b717027b16aeddd898d740d2e479c174d32f
unknownproperty <igor.buksheff@yandex.ru>
2024-09-13 23:46:00 +0300

    feat: remove webpack config

--- apps/xi.front/next.config.js
+++ apps/xi.front/next.config.js
@@ -75,15 +75,6 @@ const nextConfig = {
     ],
   },
   output: 'standalone',
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      issuer: { and: [/\.(js|ts|md)x?$/] },
-      type: 'asset/resource',
-    });
-
-    return config;
-  },
 };
 
 if (process.env.NODE_ENV !== 'development') {
