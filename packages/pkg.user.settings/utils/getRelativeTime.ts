2c1a905421f0feb64c859833621ce189e8062d32
d9m0n4 <ched08@yandex.ru>
2024-09-25 22:09:38 +0300

    fix(38988552): convert UTC date to locale timezone

--- packages/pkg.user.settings/utils/getRelativeTime.ts
+++ packages/pkg.user.settings/utils/getRelativeTime.ts
@@ -1,11 +1,13 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
 import 'dayjs/locale/ru';
 
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
 dayjs.locale('ru');
 
 export const getRelativeTime = (dateString: string): string => {
-  const date = dayjs(dateString);
+  const date = dayjs.utc(dateString).local();
   return dayjs().to(date);
 };
