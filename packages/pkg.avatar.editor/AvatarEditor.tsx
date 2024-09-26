b9d85ffb7d26092b545e15813d5ef3b007460e48
reinaka <juliabednaia@gmail.com>
2024-09-26 22:17:19 +0300

    fix(39085108): update modal package version, add scroll

--- packages/pkg.avatar.editor/AvatarEditor.tsx
+++ packages/pkg.avatar.editor/AvatarEditor.tsx
@@ -143,76 +143,80 @@ export const AvatarEditorComponent = ({
         <ModalCloseButton>
           <Close className="fill-gray-80 sm:fill-gray-0" />
         </ModalCloseButton>
-        <ModalHeader className="mb-6">
-          <ModalTitle className="xs:max-w-none max-w-[240px] leading-8">
-            Изменение фотографии
-          </ModalTitle>
-        </ModalHeader>
-        <div className="relative h-[300px] w-[calc(100%-48px)]">
-          <Cropper
-            image={
-              file ||
-              'https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000'
-            }
-            crop={crop}
-            cropSize={{ width: 236, height: 236 }}
-            zoom={zoom}
-            aspect={1}
-            cropShape="round"
-            showGrid={false}
-            onCropChange={onCropChange}
-            onCropComplete={onCropComplete}
-            onZoomChange={onZoomChange}
-            style={{
-              containerStyle: {
-                borderRadius: '8px',
-                left: '24px',
-                width: '100%',
-                height: '300px',
-              },
-            }}
-            minZoom={MIN_ZOOM}
-          />
+        <div className="rounded-4 max-h-[calc(100dvh-16px)] overflow-auto">
+          <ModalHeader className="mb-6">
+            <ModalTitle className="xs:max-w-none max-w-[240px] leading-8">
+              Изменение фотографии
+            </ModalTitle>
+          </ModalHeader>
+          <div className="relative mx-auto h-[300px] w-[calc(100%-48px)]">
+            <Cropper
+              image={
+                file ||
+                'https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000'
+              }
+              crop={crop}
+              cropSize={{ width: 236, height: 236 }}
+              zoom={zoom}
+              aspect={1}
+              cropShape="round"
+              showGrid={false}
+              onCropChange={onCropChange}
+              onCropComplete={onCropComplete}
+              onZoomChange={onZoomChange}
+              style={{
+                containerStyle: {
+                  borderRadius: '8px',
+                  width: '100%',
+                  height: '300px',
+                },
+                mediaStyle: {
+                  transform: 'none',
+                },
+              }}
+              minZoom={MIN_ZOOM}
+            />
+          </div>
+          <div className="flex h-[60px] w-full items-center justify-center px-6 pb-6 pt-4">
+            <button
+              aria-label="Минус"
+              type="button"
+              className="mx-4 bg-transparent p-1"
+              onClick={decreaseZoom}
+            >
+              <Minus size="m" />
+            </button>
+            <Slider
+              className="max-w-[250px] flex-1"
+              value={[zoom]}
+              max={MAX_ZOOM}
+              step={ZOOM_STEP}
+              min={MIN_ZOOM}
+              defaultValue={[zoom]}
+              onValueChange={(v: number[]) => onZoomChange(v[0])}
+            />
+            <button
+              aria-label="Плюс"
+              type="button"
+              className="mx-4 bg-transparent p-1"
+              onClick={increaseZoom}
+            >
+              <Plus size="m" />
+            </button>
+          </div>
+          <ModalFooter className="flex flex-col gap-4 sm:flex-row sm:items-start">
+            <Button onClick={() => showCroppedImage()} className="w-full sm:w-[126px]">
+              Изменить
+            </Button>
+            <Button
+              onClick={() => onOpenChange(false)}
+              className="w-full sm:w-[126px]"
+              variant="secondary"
+            >
+              Отменить
+            </Button>
+          </ModalFooter>
         </div>
-        <div className="flex h-[60px] w-full items-center justify-center px-6 pb-6 pt-4">
-          <button
-            aria-label="Минус"
-            type="button"
-            className="mx-4 bg-transparent p-1"
-            onClick={decreaseZoom}
-          >
-            <Minus size="m" />
-          </button>
-          <Slider
-            className="max-w-[250px] flex-1"
-            value={[zoom]}
-            max={MAX_ZOOM}
-            step={ZOOM_STEP}
-            min={MIN_ZOOM}
-            defaultValue={[zoom]}
-            onValueChange={(v: number[]) => onZoomChange(v[0])}
-          />
-          <button
-            aria-label="Плюс"
-            type="button"
-            className="mx-4 bg-transparent p-1"
-            onClick={increaseZoom}
-          >
-            <Plus size="m" />
-          </button>
-        </div>
-        <ModalFooter className="flex flex-col gap-4 sm:flex-row sm:items-start">
-          <Button onClick={() => showCroppedImage()} className="w-full sm:w-[126px]">
-            Изменить
-          </Button>
-          <Button
-            onClick={() => onOpenChange(false)}
-            className="w-full sm:w-[126px]"
-            variant="secondary"
-          >
-            Отменить
-          </Button>
-        </ModalFooter>
       </ModalContent>
     </Modal>
   );
