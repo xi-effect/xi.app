5fc0d9578ef310a7238caa5b9b40d4f5ccb79d16
d9m0n4 <ched08@yandex.ru>
2024-10-04 15:21:54 +0300

    fix(38990413): fix email confirmation route group

--- apps/xi.front/app/(common)/confirm-email/[id]/page.tsx
+++ apps/xi.front/app/(common)/confirm-email/[id]/page.tsx
@@ -1,13 +1,15 @@
+import dynamic from 'next/dynamic';
 import { EmailConfirmation } from 'pkg.email-confirmation';
-import { Logo } from 'pkg.logo';
 import React from 'react';
+
+const Logo = dynamic(() => import('pkg.logo').then((mod) => mod.Logo), { ssr: false });
 
 export default function ConfirmEmailPage() {
   return (
     <div className="flex flex-wrap flex-col justify-center content-center w-screen h-[100dvh] xs:h-screen p-1">
       <div className="flex flex-col py-12 xl:py-24 h-full px-6 w-full">
         <div className="flex justify-center items-center h-8 p-1">
-          <Logo height={16} width={134} logoVariant="navigation" logoSize="default" />
+          <Logo width={134} height={16} logoSize="default" logoVariant="navigation" />
         </div>
         <div className="shrink-0 h-full flex flex-col justify-center items-center">
           <EmailConfirmation />
--- packages/pkg.email-confirmation/EmailConfirmation.tsx
+++ packages/pkg.email-confirmation/EmailConfirmation.tsx
@@ -3,63 +3,51 @@
 import { Button } from '@xipkg/button';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
-import { post } from 'pkg.utils';
-import { useEffect, useState } from 'react';
+import { postEmailConfirm } from 'pkg.api';
+import { useCallback, useEffect, useState } from 'react';
 
 export const EmailConfirmation = () => {
   const [hasError, setHasError] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const params = useParams();
 
-  useEffect(() => {
-    const confirmEmail = async (token: string | string[]) => {
-      try {
-        const decodedToken = decodeURIComponent(token as string);
-
-        const { status } = await post({
-          service: 'auth',
-          path: '/api/email-confirmation/confirmations/',
-          body: { token: decodedToken },
-          config: {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          },
-        });
-
-        if (status !== 204) {
-          setHasError(true);
-        }
-      } catch (e) {
+  const confirmEmail = useCallback(async () => {
+    try {
+      const { status } = await postEmailConfirm(params.id[0]);
+      if (status !== 201) {
         setHasError(true);
-      } finally {
-        setLoading(false);
       }
-    };
-
-    if (params.id) {
-      confirmEmail(params.id);
+    } catch (e) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
     }
-  }, []);
+  }, [params.id]);
 
-  if (loading) {
+  useEffect(() => {
+    confirmEmail();
+  }, [confirmEmail]);
+
+  if (isLoading) {
     return <div>Загрузка...</div>;
   }
 
-  return hasError ? (
-    <div className="xs:p-8 flex max-w-[400px] flex-col items-center gap-4 p-0 text-center">
-      <h2 className="text-xl-base font-semibold">Ссылка не действительна</h2>
-      <p className="text-gray-80 text-m-base font-medium">
-        Попробуйте снова или обратитесь в поддержку
-      </p>
-    </div>
-  ) : (
+  if (hasError) {
+    return (
+      <div className="xs:p-8 flex max-w-[400px] flex-col items-center gap-4 p-0 text-center">
+        <h2 className="text-xl-base font-semibold">Ссылка не действительна</h2>
+        <p className="text-gray-80 text-m-base font-medium">
+          Попробуйте снова или обратитесь в поддержку
+        </p>
+      </div>
+    );
+  }
+
+  return (
     <div className="xs:w-auto flex w-full flex-col items-center gap-6">
       <h2 className="text-xl-base font-semibold">Почта подтверждена</h2>
       <Link href="/" passHref className="xs:w-auto w-full">
-        <Button className="xs:w-auto w-full px-8">
-          <span className="text-l-base">Продолжить</span>
-        </Button>
+        <Button className="xs:w-auto w-full px-8 text-xl">Продолжить</Button>
       </Link>
     </div>
   );
