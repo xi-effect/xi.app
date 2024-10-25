e6411cdb68667196436f3bdbf634302c239acdc2
Maxim <maksim.efremof@gmail.com>
2024-10-25 22:46:24 +0300

    fix(39526328): bottom menu layout fix

--- packages/pkg.module.tasks-admin/components/FooterCreateTask.tsx
+++ packages/pkg.module.tasks-admin/components/FooterCreateTask.tsx
@@ -9,25 +9,25 @@ type FooterCreateTaskPropsT = {
 };
 
 export const FooterCreateTask = ({ onAddTask }: FooterCreateTaskPropsT) => (
-  <div className="mt-16 flex self-center max-2xl:flex-col">
+  <div className="mt-16 flex self-center max-[1000px]:flex-col">
     Добавить вопрос:
-    <div className="ml-8 flex gap-2 max-2xl:ml-0 max-2xl:flex-col max-2xl:items-start">
+    <div className="ml-8 flex gap-2 max-[1000px]:ml-0 max-[1000px]:flex-col max-[1000px]:items-start">
       <Button
         size="s"
         variant="ghost"
-        className="p-1.5 max-2xl:mt-1 max-2xl:pl-0"
+        className="p-1.5 max-[1000px]:mt-1 max-[1000px]:pl-0"
         onClick={() => onAddTask('simple')}
       >
         <SimpleAnswer className="fill-gray-100" />
-        <span className="ml-2 max-2xl:ml-0">Простой ответ</span>
+        <span className="ml-2 max-[1000px]:ml-0">Простой ответ</span>
       </Button>
-      <Button size="s" variant="ghost" className="p-1.5 max-2xl:pl-0">
+      <Button size="s" variant="ghost" className="p-1.5 max-[1000px]:pl-0">
         <LongAnswer className="fill-gray-100" />
-        <span className="ml-2 max-2xl:ml-0">Развёрнутый ответ</span>
+        <span className="ml-2 max-[1000px]:ml-0">Развёрнутый ответ</span>
       </Button>
-      <Button size="s" variant="ghost" className="p-1.5 max-2xl:pl-0">
+      <Button size="s" variant="ghost" className="p-1.5 max-[1000px]:pl-0">
         <LongAnswer className="fill-gray-100" />
-        <span className="ml-2 max-2xl:ml-0">Выбор варианта</span>
+        <span className="ml-2 max-[1000px]:ml-0">Выбор варианта</span>
       </Button>
     </div>
   </div>
