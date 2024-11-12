178559e267e646bd448f803168dc46bd4e1f8c02
reinaka <juliabednaia@gmail.com>
2024-11-12 19:50:45 +0300

    fix(38988409): add useProvider and EditableElement, remove some console.logs

---
+++ packages/pkg.module.editor/components/EditableElement.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NodeEntry, Range } from 'slate';
+import { Editable, RenderLeafProps, RenderElementProps } from 'slate-react';
+import { Leaf } from './Leaf';
+
+export type EditableElementPropsT = {
+  readOnly?: boolean;
+  renderElement: (props: RenderElementProps) => React.JSX.Element;
+  decorate?: (entry: NodeEntry) => Range[];
+  renderLeaf: (props: RenderLeafProps) => React.JSX.Element;
+  onKeyDown?: React.KeyboardEventHandler;
+};
+
+export const EditableElement = ({
+  renderElement,
+  decorate,
+  onKeyDown,
+  readOnly,
+}: EditableElementPropsT) => (
+  <Editable
+    readOnly={readOnly || false}
+    className="flex flex-col gap-2 p-2 text-gray-100 focus-visible:outline-none focus-visible:[&_*]:outline-none"
+    renderElement={renderElement}
+    decorate={decorate}
+    renderLeaf={(props) => <Leaf {...props} />}
+    onKeyDown={onKeyDown}
+  />
+);
--- packages/pkg.module.editor/components/index.tsx
+++ packages/pkg.module.editor/components/index.tsx
@@ -2,5 +2,6 @@ export { AddNewNode } from './AddNewNode';
 export { CellControls } from './CellControls';
 export { Portal } from './CellControls';
 export { SortableElement } from './SortableElement';
+export { EditableElement } from './EditableElement';
 export { Leaf } from './Leaf';
 export { InlineToolbar } from './InlineToolbar';
