7452b9a3dff80f5d4e684b39a7bde4ef62d269c0
Kishin <aleck.germanov@yandex.ru>
2024-11-26 17:57:16 +0300

    fix(40416667): create a chat participant list component

--- packages/pkg.module.chat/ChatModule.tsx
+++ packages/pkg.module.chat/ChatModule.tsx
@@ -13,7 +13,9 @@ export const ChatModule = () => {
   return (
     <div className="flex h-full max-h-full w-full flex-row overflow-x-hidden">
       <motion.div
-        animate={{ marginRight: currentSidebar !== null ? '300px' : '0px' }} // Меняем размер шапки при открытии меню
+        animate={{
+          marginRight: currentSidebar !== null && window.innerWidth > 959 ? '300px' : '0px',
+        }} // Меняем размер шапки при открытии меню
         transition={{ type: 'tween', duration: 0.3 }}
         className="relative flex h-full w-full flex-col overflow-hidden"
       >
@@ -28,7 +30,7 @@ export const ChatModule = () => {
             animate={{ x: '0%' }}
             exit={{ x: '100%' }}
             transition={{ type: 'tween', duration: 0.3 }}
-            className="bg-green-0 z-15 fixed right-0 top-0 h-full min-w-[300px]"
+            className="bg-green-0 z-15 fixed right-0 top-0 hidden h-full min-w-[300px] md:block"
           >
             <Sidebar />
           </motion.aside>
