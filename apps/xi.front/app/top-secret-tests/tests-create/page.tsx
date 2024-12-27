6fdde848042268ccfdca30538cb8087f39758863
unknownproperty <igor.buksheff@yandex.ru>
2024-12-27 21:04:53 +0300

    fix: update versions of packages

--- apps/xi.front/app/top-secret-tests/tests-create/page.tsx
+++ apps/xi.front/app/top-secret-tests/tests-create/page.tsx
@@ -3,7 +3,9 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-const TasksCreate = dynamic(() => import('pkg.module.tasks-admin').then((mod) => mod.TasksCreate));
+const TasksCreate = dynamic(() => import('pkg.module.tasks-admin').then((mod) => mod.TasksCreate), {
+  ssr: false,
+});
 
 export default function FormPage() {
   return <TasksCreate />;
