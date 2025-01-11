ef15d7181a3381b64d15e220da7791ab0538893c
unknownproperty <igor.buksheff@yandex.ru>
2025-01-11 23:37:39 +0300

    fix(pkg.module.call): button in button error

--- packages/pkg.module.call/widgets/Up/Settings.tsx
+++ packages/pkg.module.call/widgets/Up/Settings.tsx
@@ -16,20 +16,18 @@ type SettingsPropsT = {
   children: React.ReactNode;
 };
 
-export const Settings = ({ children }: SettingsPropsT) => {
-  console.log('');
-
-  return (
-    <Sheet>
-      <SheetTrigger className="ml-2 bg-transparent">{children}</SheetTrigger>
-      <SheetContent className="bg-gray-100 p-4">
-        <SheetHeader className="flex h-10 flex-row items-center justify-between space-y-0">
-          <SheetTitle className="text-gray-0">Настройки</SheetTitle>
-          <SheetClose className="mt-0 bg-transparent pt-0">
-            <Close className="fill-gray-0" />
-          </SheetClose>
-        </SheetHeader>
-      </SheetContent>
-    </Sheet>
-  );
-};
+export const Settings = ({ children }: SettingsPropsT) => (
+  <Sheet>
+    <SheetTrigger className="ml-2" asChild>
+      {children}
+    </SheetTrigger>
+    <SheetContent className="bg-gray-100 p-4">
+      <SheetHeader className="flex h-10 flex-row items-center justify-between space-y-0">
+        <SheetTitle className="text-gray-0">Настройки</SheetTitle>
+        <SheetClose className="mt-0 bg-transparent pt-0">
+          <Close className="fill-gray-0" />
+        </SheetClose>
+      </SheetHeader>
+    </SheetContent>
+  </Sheet>
+);
