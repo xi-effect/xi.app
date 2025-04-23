f773d28d2e86ef865a079a09ed39c273f4956a6a
d9m0n4 <ched08@yandex.ru>
2025-04-23 21:32:15 +0300

    fix(48595868): update packages

--- packages/pkg.module.board/hooks/index.ts
+++ packages/pkg.module.board/hooks/index.ts
@@ -2,5 +2,5 @@ export { useZoom } from './useWheelZoom';
 export { useElementHandlers } from './useElementHandlers';
 export { useIsStageScaling } from './useIsStageScaling';
 export { useCanvasHandlers } from './useCanvasHandlers';
-export { useYjs } from './useYjs';
 export { useCursor } from './useCursor';
+export { useYjs } from './useYjs';
